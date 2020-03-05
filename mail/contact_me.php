7f1e3c89b8840c9b629f83e5ba8574f340bf1ed5
Gourav Chatterjee <chatterjeegouravking@gmail.com>
2020-03-05 23:36:26 +0530

    contact details

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+
+include '/CONFIG/config.php';
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
@@ -10,12 +12,24 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+$currDate = date("Y-m-d h:i:sa");
+
+$stmt = $link->prepare("INSERT INTO CONTACT (`NAME`, `EMAIL`, `PHONE`, `MESSAGE`, `DATE`)VALUES(?, ?, ?, ?, ?)");
+
+$stmt->bind_param("sssss", $name, $email, $phone, $message, $currdate);
+
+$result = $stmt->execute();
+if ($result) {
+	# code...
+}
+
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "chatterjeegouravking@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: noreply@codewithbogo.in\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
+echo "asdadbg";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
